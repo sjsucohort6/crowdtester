d54c44941a7124be887616e226eca9770432e9f6
rwatsh <watsh.rajneesh@sjsu.edu>
2015-10-04 11:15:31 -0700

    Initial Commit

---
+++ database/src/main/java/edu/sjsu/cohort6/crowdtester/App.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cohort6.crowdtester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
