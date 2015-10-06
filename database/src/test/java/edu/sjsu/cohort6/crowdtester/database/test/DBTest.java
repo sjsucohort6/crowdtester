472e86d5253246902fc402c7579e86d7e6652112
rwatsh <watsh.rajneesh@sjsu.edu>
2015-10-06 00:50:12 -0700

    Initial commit

---
+++ database/src/test/java/edu/sjsu/cohort6/crowdtester/database/test/DBTest.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2015 San Jose State University.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
+
+package edu.sjsu.cohort6.crowdtester.database.test;
+
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Module;
+import edu.sjsu.cohort6.crowdtester.common.model.Role;
+import edu.sjsu.cohort6.crowdtester.common.model.RoleType;
+import edu.sjsu.cohort6.crowdtester.common.model.entity.User;
+import edu.sjsu.cohort6.crowdtester.database.dao.BaseDAO;
+import edu.sjsu.cohort6.crowdtester.database.dao.DBClient;
+import edu.sjsu.cohort6.crowdtester.database.dao.DBFactory;
+import edu.sjsu.cohort6.crowdtester.database.dao.DatabaseModule;
+import edu.sjsu.cohort6.crowdtester.database.dao.mongodb.UserDAO;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.lang.reflect.ParameterizedType;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * This class is meant to be inherited from by all DB tests.
+ * It has methods for test setup and tear down.
+ *
+ * @author rwatsh on 9/24/15.
+ */
+public abstract class DBTest<T extends BaseDAO, S> {
+    @Inject
+    private DBFactory dbFactory;
+    protected T dao;
+    private Class<T> tClass;
+
+    public String dbName = "testreg";
+    private static final Logger log = Logger.getLogger(DBTest.class.getName());
+    protected static DBClient client;
+    private long startTime;
+
+    public DBTest() {
+        Module module = new DatabaseModule();
+        Guice.createInjector(module).injectMembers(this);
+
+    }
+
+    @BeforeClass
+    @Parameters({"server", "port", "dbName"})
+    public void setUp(@Optional("localhost") String server,
+                      @Optional("27017") String port,
+                      @Optional("testreg") String dbName) throws Exception {
+        client = dbFactory.create(server, Integer.parseInt(port), dbName);
+
+        this.dbName = dbName;
+        /*
+         * Use reflection to infer the class for T type.
+         */
+        this.tClass = (Class<T>) ((ParameterizedType) getClass()
+                .getGenericSuperclass()).getActualTypeArguments()[0];
+        dao = (T) client.getDAO(tClass);
+        client.dropDB(this.dbName);
+    }
+
+
+    @AfterClass
+    public void tearDown() throws Exception {
+        client.close();
+    }
+
+    @BeforeMethod
+    public void createDB() {
+        client.useDB(dbName);
+        log.info("********************");
+        startTime = System.currentTimeMillis();
+    }
+
+    @AfterMethod
+    public void dropDB() {
+        //client.dropDB(dbName);
+        long endTime = System.currentTimeMillis();
+        long diff = endTime - startTime;
+        log.info(MessageFormat.format("********* Time taken: {0} ms", diff));
+    }
+
+    /*
+     * Abstract test methods to be implemented by concrete test classes.
+     */
+    @Test
+    abstract public void testAdd() throws Exception;
+
+    @Test
+    abstract public void testRemove() throws Exception;
+
+    @Test
+    abstract public void testUpdate() throws Exception;
+
+    @Test
+    abstract public void testFetch() throws Exception;
+
+    /**
+     * Common test methods shared across test sub classes.
+     */
+
+
+    public static User testCreateUser() {
+        User user = getTestUser();
+        UserDAO userDAO = (UserDAO)client.getDAO(UserDAO.class);
+        List<User> usersList = new ArrayList<>();
+        usersList.add(user);
+        List<String> insertedIds = userDAO.add(usersList);
+        log.info("User: " + user);
+        List<User> users = userDAO.fetchById(insertedIds);
+        Assert.assertNotNull(users);
+        log.info("User created: " + users);
+        return users.get(0);
+    }
+
+    public static User getTestUser() {
+        return new User("john.doe@sjsu.edu", "john.doe@sjsu.edu", "John", "Doe", new Role(RoleType.TESTER));
+    }
+}
---
+++ database/src/test/java/edu/sjsu/cohort6/crowdtester/database/test/UserDAOTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2015 San Jose State University.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
+
+package edu.sjsu.cohort6.crowdtester.database.test;
+
+import edu.sjsu.cohort6.crowdtester.common.model.entity.User;
+import edu.sjsu.cohort6.crowdtester.database.dao.mongodb.UserDAO;
+import org.testng.Assert;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * @author rwatsh on 9/24/15.
+ */
+public class UserDAOTest extends DBTest<UserDAO, User> {
+
+    private static final Logger log = Logger.getLogger(UserDAOTest.class.getName());
+
+    @Override
+    public void testAdd() {
+        testCreateUser();
+    }
+
+    @Override
+    public void testRemove() throws Exception {
+
+    }
+
+    @Override
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Override
+    public void testFetch() throws Exception {
+        testCreateUser();
+        List<User> users = dao.fetchById(null);
+        log.info(users.toString());
+        Assert.assertNotNull(users);
+        Assert.assertTrue(!users.isEmpty(), "Could not fetch users");
+    }
+}
